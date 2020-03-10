88baf865d91876f653f994fd60488b6499fed005
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-03-10 15:55:40 -0500

    adds improved code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request');
 const argv = require('yargs').argv;
 
 let apiKey = 'eb9eb65cb209c57a9a3d21db30dc140f';
-let city = argv.c || 'portland';
+let city = argv.c || 'omaha';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
 request(url, function (err, response, body) {
--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
+
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  
-request(url, function (err, response, body) {
+
+  request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
@@ -30,6 +31,7 @@ request(url, function (err, response, body) {
     }
   });
 })
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
